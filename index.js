c9ba86c09e92a8b9f1504a3a5776e48c72fdfe57
刘启明 <455043818@qq.com>
2023-08-23 17:17:59 +0800

    feat: reset 重置

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const ci = require('miniprogram-ci');
+const { spawn } = require('child_process');
 
-const packageObject = require('../package.json');
+
+// package.json 配置获取
+const packageObject = require('./package.json');
 const version =packageObject.version;
 const lastVersion = packageObject.lastVersion;
+
+const compilePath = `e:\\work\\git-refactor\\mini-program`
+const command = 'pnpm.cmd';
+const args = ['xe'];
+
+const child = spawn(command, args, { cwd: compilePath });
+child.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+child.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+child.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+  // compile();
+});
+
+
+
 function getLength(s) {
   const match = s.match(/^(\d+)$/);
   return match ? match[0].length : null;
 }
 
-
-export const getFullVersion = () => {
+const getFullVersion = () => {
   const length = getLength(lastVersion)
 
   let strVersion = ""
@@ -22,23 +45,22 @@ export const getFullVersion = () => {
   return `${version}.${strVersion}`
 }
 
-
-const project = new ci.Project({
+const compile = () => {
+  const project = new ci.Project({
     appid: "wx636b10db5fe7f274",
     type: "miniProgram",
-    projectPath: `e:\\work\\git-refactor\\mini-program\\apps\\digital-village\\dist\\build\\mp-weixin`,
+    projectPath: `${compilePath}\\apps\\digital-village\\dist\\build\\mp-weixin`,
     privateKeyPath: "private.wx636b10db5fe7f274.key",
     ignores: ['node_modules/**/*'],
-});
-
-ci.upload(
+  });
+  ci.upload(
   {
     project,
     version: getFullVersion(),
-    desc: "糖家三勺 在 2023年8月21日晚上7点52分 提交上传",
+    desc: `自动化提交发布版本${getFullVersion()}`,
     setting: {
       es6: true,     // 对应小程序开发者工具的 "es6 转 es5"
-			es7: true,     // 对应小程序开发者工具的 "增强编译"
+      es7: true,     // 对应小程序开发者工具的 "增强编译"
       minify: true,  // 压缩所有代码，对应小程序开发者工具的 "压缩代码"
       ignoreUploadUnusedFiles: true,
     },
@@ -46,4 +68,8 @@ ci.upload(
   }).then(result => {
     console.log(result, "uploadResult");
   })
+}
+
+
+
 
